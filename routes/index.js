022d853abe961a4e82224de7029bb9e032f10395
Tom Shushan <tomshudev@gmail.com>
2018-01-06 20:19:19 +0200

    Adapting the server to support the tracks visualization

--- routes/index.js
+++ routes/index.js
@@ -6,53 +6,68 @@ var router = express.Router();
 
 function log(a){console.log(a); return a;}
 function generate(){
-  function generateNetos(bruto){
-      var lastDate = moment(bruto.start)
-      for (let index = 0; index < 30; index++) {
-          var start = moment(lastDate).add(chance().integer({min: 1, max: 10}), 'm')
-          var end = moment(start).add(chance().integer({min: 1, max: 20}),'m')
-          if (end.diff(bruto.end) > 0)
-              continue
-          var description = faker.random.words()
-          var name = faker.random.word()
-          var neto = {
-              name: name,
-              start: new Date(start),
-              end: new Date(end),
-              description:description,
-          }
-          lastDate = moment(end)
-          bruto.netosArray.push(neto)
-      }
-  }
+  var tracks = []
 
-  brutos = []
-  // generate bruto
-  for (let index = 0; index < chance().integer({min: 7, max: 10}); index++) {
-      var yesterday = moment().subtract(1,'day')
-      var now = moment()
-      var start = moment(faker.date.between(yesterday,now))
-      var end = moment(faker.date.between(start,now))
-      if (end.diff(start,'hours') < 3){
-          index --
-          continue
-      }
-      var description = faker.random.words()
-      var name = faker.random.word()
-      var bruto = {
-          name: name,
-          start: start,
-          end: end,
-          description:description,
-          netosArray: [],
-          addedToDB: moment(faker.date.between(start, end))
-      }
-      generateNetos(bruto)
-      brutos.push(bruto)
-  }
+  for (let trackId = 0; trackId < chance().integer({min: 10, max: 50}); trackId++) {
+        function generateNetos(bruto){
+            var lastDate = moment(bruto.start)
+            for (let index = 0; index < 30; index++) {
+                var start = moment(lastDate).add(chance().integer({min: 1, max: 10}), 'm')
+                var end = moment(start).add(chance().integer({min: 1, max: 20}),'m')
+                if (end.diff(bruto.end) > 0)
+                    continue
+                var description = faker.random.words()
+                var name = faker.random.word()
+                var neto = {
+                    name: name,
+                    start: new Date(start),
+                    end: new Date(end),
+                    description:description,
+                }
+                lastDate = moment(end)
+                bruto.netosArray.push(neto)
+            }
+        }
 
-  console.log(JSON.stringify(brutos))
-  return brutos
+        brutos = []
+        // generate bruto
+        for (let index = 0; index < chance().integer({min: 7, max: 10}); index++) {
+            var monthBack = moment().subtract(31,'day')
+            var yesterday = moment().subtract(1,'day')
+            var now = moment()
+            var start = moment(faker.date.between(yesterday,now))
+            var end = moment(faker.date.between(start,now))
+            if (end.diff(start,'hours') < 3){
+                index --
+                continue
+            }
+            var description = faker.random.words()
+            var name = faker.random.word()
+            var bruto = {
+                name: name,
+                start: start,
+                end: end,
+                description:description,
+                netosArray: [],
+                addedToDB: moment(faker.date.between(start, end))
+            }
+            generateNetos(bruto)
+            brutos.push(bruto)
+        }
+
+        var track = {
+            id: trackId,
+            name: faker.random.words(),
+            start: moment(faker.date.between(monthBack, now)),
+            brutos: brutos
+        }
+      
+        tracks.push(track)
+    }
+
+  console.log(JSON.stringify(tracks))
+
+  return tracks
 }
 
 router.all('/', function(req, res, next) {
